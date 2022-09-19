8079ebff7bb7b60ebcf553e69855205b4b122139
jean viven <jeanvivien.sicot@lapiscine.pro>
2022-09-19 08:02:29 +0200

    clearing code

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,6 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
-    .addEntry('AppReact', './assets/react/App/App.jsx')
     
     .addEntry('Home', './assets/react/pages/Home/Home.jsx')
     .addEntry('LegalNotice', './assets/react/pages/LegalNotice/LegalNotice.jsx')
